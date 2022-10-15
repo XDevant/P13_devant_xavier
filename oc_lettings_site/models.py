dfb97dd91a6fa492f0abfd09e8afb05ac3af32f8
XDevant <xdevant@gmail.com>
2022-10-16 01:47:03 +0200

    address model moved to lettings app successfully.

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -1,26 +1,11 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.contrib.auth.models import User
-
-
-class Address(models.Model):
-    number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
-    street = models.CharField(max_length=64)
-    city = models.CharField(max_length=64)
-    state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
-    zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
-    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
-
-    def __str__(self):
-        return f'{self.number} {self.street}'
-
-    class Meta:
-        verbose_name_plural = "addresses"
+from lettings.models import Address as Add
 
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name='old_address')
+    address = models.OneToOneField(Add, on_delete=models.CASCADE, related_name='old_address')
 
     def __str__(self):
         return self.title
