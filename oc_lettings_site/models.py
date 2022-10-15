3f96021a5f3a89fd090ea661e22804d84b3d0ee1
XDevant <xdevant@gmail.com>
2022-10-16 00:59:01 +0200

    sql command ready for app split.

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -20,14 +20,14 @@ class Address(models.Model):
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name='old_address')
 
     def __str__(self):
         return self.title
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_user')
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
