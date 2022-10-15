a72c8a160106e4a25dfa842bce008d6209c85e3a
XDevant <xdevant@gmail.com>
2022-10-15 17:25:51 +0200

    Plural for adress corrected on admin site, linting ok

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -5,7 +5,8 @@ from .models import Letting, Profile
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 # Quisque molestie quam lobortis leo consectetur ullamcorper non id est.
 # Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
-# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis.
+# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum,
+# eget consequat ipsum lobortis quis.
 # Phasellus eleifend ex auctor venenatis tempus.
 # Aliquam vitae erat ac orci placerat luctus.
 # Nullam elementum urna nisi, pellentesque iaculis enim cursus in.
@@ -14,19 +15,24 @@ def index(request):
     return render(request, 'index.html')
 
 
-# Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non placerat massa.
+# Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit.
+# Sed non placerat massa.
 # Integer est nunc, pulvinar a tempor et, bibendum id arcu.
-# Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras eget scelerisque.
+# Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+# Cras eget scelerisque.
 def lettings_index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'lettings_index.html', context)
 
 
-# Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan porta nisl id eleifend.
-# Praesent dignissim, odio eu consequat pretium, purus urna vulputate arcu, vitae efficitur lacus justo nec purus.
+# Cras ultricies dignissim purus, vitae hendrerit ex varius non.
+# In accumsan porta nisl id eleifend.
+# Praesent dignissim, odio eu consequat pretium, purus urna vulputate arcu,
+# vitae efficitur lacus justo nec purus.
 # Aenean finibus faucibus lectus at porta.
-# Maecenas auctor, est ut luctus congue, dui enim mattis enim, ac condimentum velit libero in magna.
+# Maecenas auctor, est ut luctus congue, dui enim mattis enim,
+# ac condimentum velit libero in magna.
 # Suspendisse potenti. In tempus a nisi sed laoreet.
 # Suspendisse porta dui eget sem accumsan interdum.
 # Ut quis urna pellentesque justo mattis ullamcorper ac non tellus.
@@ -43,7 +49,8 @@ def letting(request, letting_id):
     return render(request, 'letting.html', context)
 
 
-# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero pulvinar eget.
+# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex,
+# sed consequat libero pulvinar eget.
 # Fusc faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum lacus d
 def profiles_index(request):
     profiles_list = Profile.objects.all()
@@ -51,8 +58,10 @@ def profiles_index(request):
     return render(request, 'profiles_index.html', context)
 
 
-# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac laoreet neque quis, pellentesque dui.
-# Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis fringilla, eros leo tristique lacus, it.
+# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac laoreet neque quis,
+# pellentesque dui.
+# Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis fringilla,
+# eros leo tristique lacus, it.
 # Nam aliquam dignissim congue. Pellentesque habitant morbi tristique senectus et netus et males.
 def profile(request, username):
     user_profile = Profile.objects.get(user__username=username)
