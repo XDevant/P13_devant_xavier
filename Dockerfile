c391b29426945da04fd4e91165a3c8bebbe881f1
XDevant <xdevant@gmail.com>
2022-10-23 11:37:17 +0200

    dockerfile ok psycopg install ok

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,27 @@
 # syntax=docker/dockerfile:1
 
-FROM python:3.10-slim-buster
+FROM python:3.10-alpine3.16
 
-COPY ./requirements.txt /requirements.txt
-RUN mkdir /oc-lettings
+ENV PYTHONNUMBUFFERED 1
+
+COPY ./requirements.txt ./requirements.txt
 COPY ./oc-lettings oc-lettings
 WORKDIR /oc-lettings
 
 RUN python -m venv venv
 ENV VIRTUAL_ENV /venv
 ENV PATH /venv/bin:$PATH
-RUN pip install --upgrade pip
-RUN pip install -r /requirements.txt
+
+RUN pip install --upgrade pip && \
+    apk add --update --no-cache postgresql-client && \
+    apk add --update --no-cache --virtual .tmp-deps \
+        build-base postgresql-dev musl-dev linux-headers && \
+    pip install -r /requirements.txt && \
+    apk del .tmp-deps && \
+    adduser --disabled-password --no-create-home app
 
 EXPOSE 8000
+
+USER app
 
 CMD [ "pyton", "manage.py", "runserver", "8000" ]
---
+++ oc-lettings/oc_lettings_site/management/commands/wait_for_db.py
@@ -0,0 +1,33 @@
+"""
+Django command to wait for the database to be available.
+We want to make sure Docker compose does not start migrations
+before db is ready.
+"""
+import time
+
+from psycopg2 import OperationalError as PsycopgOperationalError
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+    help = 'Django command to wait for database during containerization.'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (PsycopgOperationalError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
