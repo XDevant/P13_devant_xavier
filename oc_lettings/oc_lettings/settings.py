d1ea0d467c54e860d30e302dc8e84db7729b7b90
XDevant <xdevant@gmail.com>
2022-10-31 13:50:16 +0100

    new build ok , test new compose

--- oc_lettings/oc_lettings/settings.py
+++ oc_lettings/oc_lettings/settings.py
@@ -35,7 +35,7 @@ sentry_sdk.init(
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SENTRY_DNS", env('SECRET_KEY'))
+SECRET_KEY = os.environ.get("SECRET_KEY", env('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', env('DEBUG'))
