16a2c4038d0a871e5aef496c887176b03f6b4f55
XDevant <xdevant@gmail.com>
2022-11-01 03:10:36 +0100

    debug deploy

--- oc_lettings/oc_lettings/settings.py
+++ oc_lettings/oc_lettings/settings.py
@@ -39,11 +39,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY", env('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', env('DEBUG'))
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oc-lettings-oc']
 
 if IS_HEROKU:
     DEBUG = True
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['localhost', 'oc-lettings-oc']
 
 # Application definition
 
