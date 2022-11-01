5ff40b960496a1b203a0ff98756073b5d219fc24
XDevant <xdevant@gmail.com>
2022-11-01 03:44:01 +0100

    debug deploy

--- oc_lettings/oc_lettings/settings.py
+++ oc_lettings/oc_lettings/settings.py
@@ -95,7 +95,6 @@ WSGI_APPLICATION = 'oc_lettings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -103,7 +102,8 @@ DATABASES = {
     }
 }
 if IS_HEROKU or os.environ.get('DATABASE_URL', None):
-    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
