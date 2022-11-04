ed123ee4f9cc081c68097779460e8debd162f410
XDevant <xdevant@gmail.com>
2022-11-04 10:16:13 +0100

    test update

--- oc_lettings/oc_lettings_site/views.py
+++ oc_lettings/oc_lettings_site/views.py
@@ -11,5 +11,4 @@ from django.shortcuts import render
 # Nullam elementum urna nisi, pellentesque iaculis enim cursus in.
 # Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
-    a /= 0
     return render(request, 'oc_lettings_site/index.html')
